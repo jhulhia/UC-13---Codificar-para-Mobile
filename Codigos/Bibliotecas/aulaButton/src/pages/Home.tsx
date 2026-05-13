dca604a1bbd345e031208e29cae922b392b0f581
gilmariosantos1 <gilmariosantos1@gmail.com>
2026-05-12 21:39:23 -0300

    Implementando o botão salvar
    implementado.

--- Codigos/Bibliotecas/aulaButton/src/pages/Home.tsx
+++ Codigos/Bibliotecas/aulaButton/src/pages/Home.tsx
@@ -1,25 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React from 'react';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import "./Home.css";
 
-const Home: React.FC = () => {
+function Example() {
   return (
-    <IonPage>
+    <>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Header</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <h1>Content</h1>
+        <IonButton>Default</IonButton>
+        <IonButton disabled={true}>Disabled</IonButton>
       </IonContent>
-    </IonPage>
+    </>
   );
-};
-
-export default Home;
+}
+export default Example;
