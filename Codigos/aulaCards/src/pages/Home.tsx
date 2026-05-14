6c12e7b9d10edf43aba5ae0c185f594d64e56f96
gilmariosantos1 <gilmariosantos1@gmail.com>
2026-05-13 21:45:19 -0300

    Atualizando o Card

--- Codigos/aulaCards/src/pages/Home.tsx
+++ Codigos/aulaCards/src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import CardComponent from '../components/Cards';
 
 const Home: React.FC = () => {
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,14 +13,19 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <CardComponent />
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+
+        
+
       </IonContent>
-    </IonPage>
+      
+      </IonPage>
   );
 };
 
